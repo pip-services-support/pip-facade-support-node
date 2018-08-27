c775943d8da4d671c3722208044c8926b81ae048
Dmitriy Krayniy <krdima92@gmail.com>
2018-08-27 13:16:37 +0300

    upgraded to pip.services 3.0

--- obj/src/build/SupportFacadeFactory.js
+++ obj/src/build/SupportFacadeFactory.js
@@ -1,18 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const pip_services_components_node_1 = require("pip-services-components-node");
 const pip_services_commons_node_1 = require("pip-services-commons-node");
-const pip_services_commons_node_2 = require("pip-services-commons-node");
 const AnnouncementsOperationsV1_1 = require("../operations/version1/AnnouncementsOperationsV1");
 const FeedbacksOperationsV1_1 = require("../operations/version1/FeedbacksOperationsV1");
-class SupportFacadeFactory extends pip_services_commons_node_1.Factory {
+class SupportFacadeFactory extends pip_services_components_node_1.Factory {
     constructor() {
         super();
         this.registerAsType(SupportFacadeFactory.AnnouncementsOperationsV1Descriptor, AnnouncementsOperationsV1_1.AnnouncementsOperationsV1);
         this.registerAsType(SupportFacadeFactory.FeedbacksOperationsV1Descriptor, FeedbacksOperationsV1_1.FeedbacksOperationsV1);
     }
 }
-SupportFacadeFactory.Descriptor = new pip_services_commons_node_2.Descriptor("pip-facade-support", "factory", "default", "default", "1.0");
-SupportFacadeFactory.AnnouncementsOperationsV1Descriptor = new pip_services_commons_node_2.Descriptor("pip-facade-support", "operations", "announcements", "*", "1.0");
-SupportFacadeFactory.FeedbacksOperationsV1Descriptor = new pip_services_commons_node_2.Descriptor("pip-facade-support", "operations", "feedbacks", "*", "1.0");
+SupportFacadeFactory.Descriptor = new pip_services_commons_node_1.Descriptor("pip-facade-support", "factory", "default", "default", "1.0");
+SupportFacadeFactory.AnnouncementsOperationsV1Descriptor = new pip_services_commons_node_1.Descriptor("pip-facade-support", "operations", "announcements", "*", "1.0");
+SupportFacadeFactory.FeedbacksOperationsV1Descriptor = new pip_services_commons_node_1.Descriptor("pip-facade-support", "operations", "feedbacks", "*", "1.0");
 exports.SupportFacadeFactory = SupportFacadeFactory;
 //# sourceMappingURL=SupportFacadeFactory.js.map
